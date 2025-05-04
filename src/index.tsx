432f5107775c0236b817bd15275612c124a65a7f
Elgiz Mamedov <forworkandlesson1123@gmail.com>
2025-05-04 12:41:38 +0100

    change

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-      <BrowserRouter>
+      <BrowserRouter basename="/forfun777">
       <App />
       </BrowserRouter>
   </React.StrictMode>
